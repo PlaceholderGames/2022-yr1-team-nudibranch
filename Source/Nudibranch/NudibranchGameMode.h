06b6be05e42b372532546524983c9a513df6e27c
Banachowski M (FCES) <30043856@students.southwales.ac.uk>
2022-02-14 17:00:24 +0000

    Created the unreal engine project.

---
+++ Source/Nudibranch/NudibranchGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NudibranchGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANudibranchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANudibranchGameMode();
+};
+
+
+
